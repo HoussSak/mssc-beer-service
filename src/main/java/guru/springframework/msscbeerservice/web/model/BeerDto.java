19fd725f7c76c6fcae202698a8697258c8101973
55154M <houssem.sakher@maif.fr>
2023-06-20 09:49:05 +0200

    adding web data model. Closes #2

---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -0,0 +1,20 @@
+package guru.springframework.msscbeerservice.web.model;
+
+import lombok.Builder;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+@Builder
+public record BeerDto (UUID id,
+        Integer version,
+        OffsetDateTime createdDate,
+        OffsetDateTime lastModifiedDate,
+        String beerName,
+        BeerStyleEnum beerStyle,
+        Long upc,
+        BigDecimal price,
+        Integer quantityOnHand
+) {
+
+}
---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package guru.springframework.msscbeerservice.web.model;
+
+public enum BeerStyleEnum {
+    ALE, GOSE, WHEAT, IPA, PILSNER, LAGER, STOUT, PALE, SAISON
+}
