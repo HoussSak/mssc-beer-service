917d99c03b458fb465bd62264ac830a1cc4765b4
55154M <houssem.sakher@maif.fr>
2023-07-14 21:03:44 +0200

    Fix Jackson Configuration. Closes #22

--- src/main/java/guru/springframework/msscbeerservice/events/BeerEvent.java
+++ src/main/java/guru/springframework/msscbeerservice/events/BeerEvent.java
@@ -1,14 +1,13 @@
 package guru.springframework.msscbeerservice.events;
 
 import guru.springframework.msscbeerservice.web.model.BeerDto;
-import lombok.Builder;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 @Data
-@RequiredArgsConstructor
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BeerEvent {
 
-    private final BeerDto beerDto;
+    private BeerDto beerDto;
 }
